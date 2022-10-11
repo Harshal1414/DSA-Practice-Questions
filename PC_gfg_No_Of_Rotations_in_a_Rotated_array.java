d124b0862c87c3c4d252c88dd9e7ed5fdef6d7ee
Harshal1414 <harshalpatel1414@gmail.com>
2022-10-11 22:13:38 +0530

    LC_Practice_Ques

---
+++ PC_gfg_No_Of_Rotations_in_a_Rotated_array.java
@@ -0,0 +1,19 @@
+
+class Solution{
+	int start = 0;
+	int end = arr.length - 1;
+	int target = arr[start];
+	while(start<=end){
+		int mid = start + ((end - start)/2);
+		if(target < arr[mid]){
+			end = mid-1;
+		}
+		else if (target > arr[mid]{
+			start = mid + 1;
+		}
+		else {
+			return mid;
+		}
+	}
+	return -1;
+}
