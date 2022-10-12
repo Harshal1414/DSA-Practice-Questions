5f47e9aef9ee63e5ac6c1af884fc158f942f0253
Harshal1414 <harshalpatel1414@gmail.com>
2022-10-12 19:30:29 +0530

    LC_Practice_Ques

---
+++ LC_Unique_Morse_Code_Words.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+
+public class LC_Unique_Morse_Code_Words {
+    public int uniqueMorseRepresentations(String[] words) {
+        String [] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        HashSet<String> unique_codes = new HashSet<>();
+
+        for(String word : words){
+            StringBuilder transformation = new StringBuilder();
+
+            // now we have to store character array of each word into StringBuilder.
+            for(char c : word.toCharArray()){
+                transformation.append(morse[c-'a']);
+            }
+
+            unique_codes.add(transformation.toString());
+        }
+        return unique_codes.size();
+    }
+}
+
+// HashSet are used just because it always contains unique values.
