2271c5c84016a679e21aa3a802b5c641ab85e248
Harshal1414 <harshalpatel1414@gmail.com>
2023-02-26 17:54:39 +0530

    Time : 0ms  ;  Space : 51.3mb

--- LC_238_Product_of_Array_Except_Self.java
+++ LC_238_Product_of_Array_Except_Self.java
@@ -1,17 +1,20 @@
 public class LC_238_Product_of_Array_Except_Self{
     public int[] productExceptSelf(int[] nums) {
-        int[] ans = new int[nums.length];
-        int left = 0, right = nums.length - 1, var1 = 1, var2 = 1;
-        for(int i = 0; i < nums.length; i++){
-            while(left < i){
-                var1 = var1 * nums[left];
-                left++;
-            }
-            while(right > i){
-                var2 = var2 * nums[right];
-                right--;
-            }
-            ans[i] = var1 * var2;
+        int n = nums.length;
+        int[] ans = new int[n];
+        int pre[] = new int[n];
+        int suff[] = new int[n];
+        pre[0] = 1;
+        suff[n - 1] = 1;
+
+        for(int i = 1; i < n; i++){
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--){
+            suff[i] = suff[i + 1] * nums[i + 1];
+        }
+        for(int i = 0; i < n; i++){
+            ans[i] = pre[i] * suff[i];
         }
         return ans;
     }
