3762888f82280b06a83196cf2d1a983cfd25fe20
Harshal1414 <harshalpatel1414@gmail.com>
2023-03-03 16:06:36 +0530

    Time : 1ms  ;  Space : 40.3mb

---
+++ LC_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -0,0 +1,17 @@
+public class LC_28_Find_the_Index_of_the_First_Occurrence_in_a_String {
+    public int strStr(String haystack, String needle) {
+        int haylength=haystack.length();
+        int needlelength=needle.length();
+        if(haylength<needlelength)
+            return -1;
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            int j=0;
+            while(j<needle.length() && haystack.charAt(i+j)==needle.charAt(j))
+                j++;
+            if(j==needle.length()){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
