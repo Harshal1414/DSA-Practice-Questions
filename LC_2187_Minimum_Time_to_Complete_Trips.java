899bddcabfa51184d3813682aed4af8ad630e0ef
Harshal1414 <harshalpatel1414@gmail.com>
2023-03-07 18:02:18 +0530

    Time : 205ms  ;  Space : 55.4mb

--- LC_2187_Minimum_Time_to_Complete_Trips.java
+++ LC_2187_Minimum_Time_to_Complete_Trips.java
@@ -8,13 +8,17 @@ public class LC_2187_Minimum_Time_to_Complete_Trips {
         long right = totalTrips * min;
         while(left < right){
             long mid = left + (right - left) / 2;
+            // How many trip we can do
             long total = totalTripInGivenTime(time, mid);
+            // We can not reach totalTrips, means mid is too small 
             if(total < totalTrips){
                 left = mid + 1;
             }
             else{
                 right = mid;
             }
+            // We can reach totalTrips, but we want right to be as small as possible
+            // Therefore, we don't need to + 1
         }
         return left;
     }
