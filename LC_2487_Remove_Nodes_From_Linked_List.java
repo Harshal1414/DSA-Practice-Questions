774cac44cc49accde7e5dc19e4a6a71018f94407
Harshal1414 <harshalpatel1414@gmail.com>
2023-04-14 15:39:46 +0530

    Time : 203ms  ;  Space : 63.4mb

---
+++ LC_2487_Remove_Nodes_From_Linked_List.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class LC_2487_Remove_Nodes_From_Linked_List {
+    public ListNode removeNodes(ListNode head) {
+        Stack<Integer> st = new Stack<>();
+        while(head != null){
+            if(st.size() == 0){
+                st.add(head.val);
+            }
+            else{
+                int curr = head.val;
+                while(!st.isEmpty() && st.peek() < curr){
+                    st.pop();
+                }
+                st.push(curr);
+            }
+            head = head.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = null;
+
+        while(!st.isEmpty()){
+            curr = new ListNode(st.pop());
+            curr.next = prev;  // reversing a LinkedList
+            prev = curr;
+        }
+        return curr;
+    }
+}
