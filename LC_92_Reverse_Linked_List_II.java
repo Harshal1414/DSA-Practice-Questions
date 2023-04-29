f6b1398a2d177ee0fe45e80ee15a6e35978b5902
Harshal1414 <harshalpatel1414@gmail.com>
2023-04-29 23:01:58 +0530

    Time : 0ms  ;  Space : 40.3mb

---
+++ LC_92_Reverse_Linked_List_II.java
@@ -0,0 +1,26 @@
+public class LC_92_Reverse_Linked_List_II {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null) return null;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+
+        for(int i = 0; i < left - 1; i++){
+            pre = pre.next;
+        }
+
+        ListNode start = pre.next;
+        ListNode then = start.next;
+
+        for(int i = 0; i < right - left; i++){
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+
+        return dummy.next;
+    }
+}
